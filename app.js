9acba88c7143d37e1feebaa31b131da5f347e9fc
rajanSoni0 <160305678+rajanSoni0@users.noreply.github.com>
2025-01-07 22:56:10 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,88 @@
+import { countryList } from './codes.js';
+
+const BASE_URL = "https://open.er-api.com/v6/latest";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const swapIcon = document.querySelector(".fa-arrow-right-arrow-left");
+
+const populateSelectors = () => {
+  for (let select of dropdowns) {
+    for (let currCode in countryList) {
+      let newOption = document.createElement("option");
+      newOption.innerText = `${countryList[currCode][0]} (${currCode})`;
+      newOption.value = currCode;
+      if (select.name === "from" && currCode === "USD") {
+        newOption.selected = "selected";
+      } else if (select.name === "to" && currCode === "INR") {
+        newOption.selected = "selected";
+      }
+      select.append(newOption);
+    }
+
+    select.addEventListener("change", (evt) => {
+      updateFlag(evt.target);
+    });
+  }
+};
+
+const updateExchangeRate = async () => {
+  try {
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+      amtVal = 1;
+      amount.value = "1";
+    }
+    const URL = `${BASE_URL}/${fromCurr.value}`;
+    let response = await fetch(URL);
+    
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    
+    let data = await response.json();
+    
+    if (!data.rates[toCurr.value]) {
+      throw new Error("Invalid data received from API");
+    }
+    
+    let rate = data.rates[toCurr.value];
+    let finalAmount = amtVal * rate;
+    msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+  } catch (error) {
+    msg.innerText = "Failed to fetch the exchange rate. Please try again later.";
+    console.error("Error fetching exchange rate:", error);
+  }
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode][1];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+// Swap the selected currencies
+swapIcon.addEventListener("click", () => {
+  const temp = fromCurr.value;
+  fromCurr.value = toCurr.value;
+  toCurr.value = temp;
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
+  updateExchangeRate();
+});
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+  populateSelectors();
+  updateExchangeRate();
+});
